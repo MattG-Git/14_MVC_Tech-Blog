e275682fa0014f3ea6c70c8af153f4462515524d
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-09-29 14:30:06 -0700

    added stylization with css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,7 +62,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
       attributes: { exclude: ['password'] },
       include: [
         { model: Post }, 
-      //  { model: Comment }
+        // { model: Comment }
       ],
     });
 
